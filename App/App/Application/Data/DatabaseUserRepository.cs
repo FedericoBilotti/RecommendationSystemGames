3ca7d13e6fae15738cc694a88d0cd0a14cf00aaf
Fede <fedebilotti2001@gmail.com>
2025-05-26 20:41:43 -0300

    refac: move search users from AuthenticateUserUseCase to DatabaseUserRepository

---
+++ App/App/Application/Data/DatabaseUserRepository.cs
@@ -0,0 +1,21 @@
+using App.Application.Interfaces;
+using App.Application.UseCases;
+using App.Domain.Entities;
+using App.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Application.Data;
+
+public class DatabaseUserRepository : IUserRepository
+{
+    public async Task<User?> GetUserById(AppDbContext context, Guid userId)
+    {
+        return await context.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+    }
+
+    public async Task<User?> GetUserByUsername(AppDbContext context, UserDto requestUserDto)
+    {
+        string usernameLower = requestUserDto.Username.ToLower();
+        return await context.Users.FirstOrDefaultAsync(u => u.Username == usernameLower);
+    }
+}
--- App/App/Application/UseCases/AuthenticateUserUseCase.cs
+++ App/App/Application/UseCases/AuthenticateUserUseCase.cs
@@ -6,26 +6,28 @@ using App.Application.Interfaces;
 using App.Domain.Entities;
 using App.Infrastructure.Database;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace App.Application.UseCases;
 
-public class AuthenticateUserUseCase(AppDbContext context, IConfiguration configuration) : IAuthService
+public class AuthenticateUserUseCase(
+        IConfiguration configuration, 
+        AppDbContext context, 
+        IUserRepository userRepository, 
+        IPasswordHasher<User> hasher, 
+        ITokenService tokens) 
+        : IAuthService
 {
-    
-    
     public async Task<User?> RegisterAsync(UserDto requestUserDto)
     {
-        if (await SearchUserByUsername(requestUserDto) != null)
-            return null;
+        if (await userRepository.GetUserByUsername(context, requestUserDto) != null) return null;
 
         var user = new User();
-        
+
         string hashedPassword = new PasswordHasher<User>().HashPassword(user, requestUserDto.Password);
         user.Username = requestUserDto.Username;
         user.HashedPassword = hashedPassword;
-        
+
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
@@ -34,26 +36,23 @@ public class AuthenticateUserUseCase(AppDbContext context, IConfiguration config
 
     public async Task<TokenResponseDto?> LoginAsync(UserDto requestUserDto)
     {
-        var user = await SearchUserByUsername(requestUserDto);
+        var user = await userRepository.GetUserByUsername(context, requestUserDto);
 
-        if (user == null) 
-            return null;
+        if (user == null) return null;
 
         PasswordVerificationResult passwordVerificationResult = new PasswordHasher<User>().VerifyHashedPassword(user, user.HashedPassword, requestUserDto.Password);
-        
-        if (passwordVerificationResult == PasswordVerificationResult.Failed)
-            return null;
-        
+
+        if (passwordVerificationResult == PasswordVerificationResult.Failed) return null;
+
         return await CreateTokenResponse(user);
     }
 
     public async Task<TokenResponseDto?> RefreshTokenAsync(RefreshTokenRequestDto requestRefreshTokenDto)
     {
         User? user = await ValidateRefreshToken(requestRefreshTokenDto.UserId, requestRefreshTokenDto.RefreshToken);
-        
-        if (user == null)
-            return null;
-        
+
+        if (user == null) return null;
+
         return await CreateTokenResponse(user);
     }
 
@@ -69,9 +68,9 @@ public class AuthenticateUserUseCase(AppDbContext context, IConfiguration config
 
     private async Task<User?> ValidateRefreshToken(Guid userId, string refreshToken)
     {
-        User? user = await SearchUserById(userId);
+        User? user = await userRepository.GetUserById(context, userId);
         bool isNotValid = user == null || user.RefreshToken != refreshToken || user.RefreshTokenExpirationTime < DateTime.UtcNow;
-        
+
         return isNotValid ? null : user;
     }
 
@@ -100,28 +99,12 @@ public class AuthenticateUserUseCase(AppDbContext context, IConfiguration config
             new(ClaimTypes.NameIdentifier, user.UserId.ToString()),
             new(ClaimTypes.Role, user.Role)
         };
-        
+
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
-        var tokenDescriptor = new JwtSecurityToken(
-                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
-                audience: configuration.GetValue<string>("AppSettings:Audience"),
-                claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(30),
-                signingCredentials: credentials
-        );
-        
-        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
-    }
-    
-    private async Task<User?> SearchUserById(Guid id)
-    { 
-        return await context.Users.FirstOrDefaultAsync(u => u.UserId == id);
-    }
+        var tokenDescriptor = new JwtSecurityToken(issuer: configuration.GetValue<string>("AppSettings:Issuer"), audience: configuration.GetValue<string>("AppSettings:Audience"), claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30), signingCredentials: credentials);
 
-    private async Task<User?> SearchUserByUsername(UserDto requestUserDto)
-    {
-        string usernameLower = requestUserDto.Username.ToLower(); 
-        return await context.Users.FirstOrDefaultAsync(u => u.Username == usernameLower);
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
     }
 }
