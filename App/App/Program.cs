6c55b7edf2c75a85b01b72662da4e1aae990063a
Fede <fedebilotti2001@gmail.com>
2025-05-29 16:21:25 -0300

    chore: elimination of unnecessary packages

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,23 +1,6 @@
-using System.Text;
-using App;
-using App.RM.Application.Interfaces;
-using App.RM.Application.Interfaces.Authentication;
-using App.RM.Application.Interfaces.Engine;
-using App.RM.Application.UseCases.Authentication;
-using App.RM.Application.UseCases.Engine;
-using App.RM.Domain.Entities;
-using App.RM.Infrastructure.Data;
 using App.RM.Infrastructure.Database;
-using App.RM.Infrastructure.Services;
-using App.RM.Infrastructure.Services.Authenticate;
-using App.RM.Infrastructure.Services.Engine;
 using App.StartUp;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Scalar.AspNetCore;
-using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
