9dac2f2cb115e670428e1f8fd0daa217c9723dc3
Fede <fedebilotti2001@gmail.com>
2025-05-29 15:19:16 -0300

    feat: added range to limit value for dto game gener

--- App/App/RM.Application/Dtos/Engine/GameGenreFilterRequestDto.cs
+++ App/App/RM.Application/Dtos/Engine/GameGenreFilterRequestDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.RM.Application.Dtos.Engine;
 
 public class GameGenreFilterRequestDto
 {
-    [FromQuery(Name = "genres")] public string genre { get; set; } = string.Empty;
-    [FromQuery(Name = "limit")] public int limit { get; set; } = 1;
+    // Better to give an enum with genres, for prevent human errors
+    [FromQuery(Name = "genres"), Required] public string genre { get; set; } = string.Empty;
+    
+    [FromQuery(Name = "limit"), Required, Range(1, 50, ErrorMessage = "Limit must be between 1 and 50")]
+    public int limit { get; set; } = 20;
 }
