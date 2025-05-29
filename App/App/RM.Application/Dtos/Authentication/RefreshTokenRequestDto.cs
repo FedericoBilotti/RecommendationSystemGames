1bfb91009d23e15bca03f93049e6a1c2c2a6dff2
Fede <fedebilotti2001@gmail.com>
2025-05-29 14:33:11 -0300

    refac(Dtos): now de dtos has validation annotations instead of the entities

--- App/App/RM.Application/Dtos/Authentication/RefreshTokenRequestDto.cs
+++ App/App/RM.Application/Dtos/Authentication/RefreshTokenRequestDto.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace App.RM.Application.Dtos.Authentication;
 
 public class RefreshTokenRequestDto
 {
-    public Guid UserId { get; set; }
-    public required string RefreshToken { get; set; }
+    [Required] public Guid UserId { get; set; }
+    [Required] public required string RefreshToken { get; set; }
 }
--- App/App/RM.Application/Dtos/Authentication/UserRequestDto.cs
+++ App/App/RM.Application/Dtos/Authentication/UserRequestDto.cs
@@ -4,7 +4,7 @@ namespace App.RM.Application.Dtos.Authentication;
 
 public class UserRequestDto
 {
-    [Required] public string Username { get; set; } = string.Empty;
-    [Required] public string Password { get; set; } = string.Empty;
+    [Required, StringLength(30)] public string Username { get; set; } = string.Empty;
+    [Required, StringLength(50)] public string Password { get; set; } = string.Empty;
     [Required, StringLength(255), EmailAddress] public string Email { get; set; } = string.Empty;
 }
