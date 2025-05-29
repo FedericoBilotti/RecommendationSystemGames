74f06e00ddd731281a10607acf9e15d8a01cdb82
Fede <fedebilotti2001@gmail.com>
2025-05-29 14:56:07 -0300

    fix: error with the query name of limit

--- App/App/RM.Application/Dtos/Engine/GameGenreFilterRequestDto.cs
+++ App/App/RM.Application/Dtos/Engine/GameGenreFilterRequestDto.cs
@@ -5,5 +5,5 @@ namespace App.RM.Application.Dtos.Engine;
 public class GameGenreFilterRequestDto
 {
     [FromQuery(Name = "genres")] public string genre { get; set; } = string.Empty;
-    [FromQuery(Name = "genres")] public int limit { get; set; } = 1;
+    [FromQuery(Name = "limit")] public int limit { get; set; } = 1;
 }
